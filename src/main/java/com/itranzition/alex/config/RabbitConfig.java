b79d68176b58ad5857df45ea74f40c78306eb873
alexandr <alexzhalnerchikz@mail.ru>
2021-09-23 20:26:08 +0300

    delete mappings, delete .* imports

--- src/main/java/com/itranzition/alex/config/RabbitConfig.java
+++ src/main/java/com/itranzition/alex/config/RabbitConfig.java
@@ -1,7 +1,10 @@
 package com.itranzition.alex.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
--- src/main/java/com/itranzition/alex/mapper/UserMapper.java
+++ src/main/java/com/itranzition/alex/mapper/UserMapper.java
@@ -6,22 +6,21 @@ import com.itranzition.alex.model.dto.impl.ResponseSignUpDto;
 import com.itranzition.alex.model.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mappings({@Mapping(target = "email", source = "signUpDto.email"),
-            @Mapping(target = "name", source = "signUpDto.name"),
-            @Mapping(target = "password", source = "signUpDto.password"),
-            @Mapping(target = "surname", source = "signUpDto.surname", defaultValue = "NULL")})
+    @Mapping(target = "email", source = "signUpDto.email")
+    @Mapping(target = "name", source = "signUpDto.name")
+    @Mapping(target = "password", source = "signUpDto.password")
+    @Mapping(target = "surname", source = "signUpDto.surname", defaultValue = "NULL")
     User signUpDtoToUser(SignUpDto signUpDto);
 
-    @Mappings({@Mapping(target = "email", source = "signUpDto.email"),
-            @Mapping(target = "name", source = "signUpDto.name")})
+    @Mapping(target = "email", source = "signUpDto.email")
+    @Mapping(target = "name", source = "signUpDto.name")
     ResponseSignUpDto signUpDtoToResponseSignUpDto(SignUpDto signUpDto);
 
-    @Mappings({@Mapping(target = "email", source = "user.email"),
-            @Mapping(target = "name", source = "user.name")})
+    @Mapping(target = "email", source = "user.email")
+    @Mapping(target = "name", source = "user.name")
     RegisteredUserDto userToRegisteredUserDto(User user);
 }
