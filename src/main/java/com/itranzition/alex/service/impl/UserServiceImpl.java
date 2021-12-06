170f7fc3b26f27a2e0ca5b6379dc410eecdfa05c
alexandr <alexzhalnerchikz@mail.ru>
2021-12-06 13:09:03 +0300

    change formation on findUserByEmail

--- src/main/java/com/itranzition/alex/service/impl/UserServiceImpl.java
+++ src/main/java/com/itranzition/alex/service/impl/UserServiceImpl.java
@@ -36,8 +36,9 @@ public class UserServiceImpl implements UserService {
         if (email == null || email.isBlank()) {
             throw new BadCredentialsException(HttpStatus.BAD_REQUEST + " email cannot be null or empty");
         }
-        return userRepository.findByEmail(email).orElseThrow(() -> new BadCredentialsException(HttpStatus.BAD_REQUEST
-                + String.format(" User with email %s do not exist", email)));
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new BadCredentialsException(HttpStatus.BAD_REQUEST
+                        + String.format(" User with email %s do not exist", email)));
     }
 
     @Override
