6964201ce9114da29238ed0030ec7b92ce380ce3
alexandr <alexzhalnerchikz@mail.ru>
2021-12-06 12:45:25 +0300

    formatted user

--- src/main/java/com/itranzition/alex/model/entity/User.java
+++ src/main/java/com/itranzition/alex/model/entity/User.java
@@ -6,10 +6,10 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
